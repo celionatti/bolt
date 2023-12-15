7f18c5bc80df9a05047d83410d5dbfb264a805d6
celionatti <amisuusman@gmail.com>
2023-12-15 04:26:42 -0800

    key

--- Configs/load.php
+++ Configs/load.php
@@ -2,8 +2,16 @@
 
 declare(strict_types=1);
 
+/**
+ * Framework: PhpStrike
+ * Author: Celio Natti
+ * version: 1.0.0
+ * Year: 2023
+ * 
+ * Description: This file is for Bolt global constants
+ */
 
-$key = "";
+$key = "2c0b2fdf89bd3455b81b4ff409578d1b7bc3a067147631db9d5486b9e95024fa";
 
 if (!defined('BOLT_APP_KEY')) {
     define('BOLT_APP_KEY', $key);
